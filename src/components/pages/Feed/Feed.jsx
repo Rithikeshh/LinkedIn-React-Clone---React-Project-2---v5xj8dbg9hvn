48db2f07b79fbec944565da564c5f9e7659ec4c0
Rithikeshh <rithikeshh@gmail.com>
2023-12-21 02:14:04 +0530

    called posts  API

--- src/components/pages/Feed/Feed.jsx
+++ src/components/pages/Feed/Feed.jsx
@@ -4,19 +4,38 @@ import Loader from '../../Loader/Loader'
 
 function Feed() {
 
-    const [loader, setLoader] = useState(false)
-    const [posts, setPosts] = useState([])
-    
-    useEffect(()=>{
-        
-    },[])
+  const [loader, setLoader] = useState(true)
+  const [posts, setPosts] = useState([])
+  const getPosts = async () => {
+
+    const config = {
+      method: 'GET',
+      headers: {
+        'projectID': 'f104bi07c490'
+      }
+    }
+    try {
+      const response = await fetch("https://academics.newtonschool.co/api/v1/linkedin/post?limit=1000", config)
+      const result = await response.json()
+      setPosts(result.data)
+    } catch (error) {
+      console.log(error);
+    } finally{
+      setLoader(false)
+    }
+  }
+  document.body.style.backgroundColor = "#f4f2ee"
+  useEffect(() => {
+    getPosts()
+  }, [])
   return (
-    loader ? 
-    <Loader/>
-    :
-    <div>
-      <Navbar/>
-    </div>
+    loader ?
+      <Loader />
+      :
+      <div>
+        <Navbar />
+        
+      </div>
   )
 }
 
