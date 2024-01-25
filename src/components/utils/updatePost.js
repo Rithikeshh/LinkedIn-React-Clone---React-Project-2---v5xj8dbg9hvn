f29b774cea2e03a6a2dc4b9225da9ffb7653882d
Rithikeshh <rithikeshh@gmail.com>
2024-01-26 02:41:02 +0530

    updated

--- src/components/utils/updatePost.js
+++ src/components/utils/updatePost.js
@@ -25,7 +25,7 @@ export default async function updatePost(id, postTitle, postContent, imagePrevie
             }
 
         )
-        console.log(response);
+        
         const send = response.data.data._id;
         if(send){
             setPosts([])
